c43f7de1797a21b997bdcaefd9d1e288f2eb0eb9
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2022-12-12 08:23:36 +0530

    changed files and organized code

---
+++ tweety_lib_imports.py
@@ -0,0 +1,20 @@
+#necessary lib. imports required for Tweety - The virtual voice assistant to work.
+import ctypes
+import datetime
+import json
+import os
+import random
+import smtplib
+import subprocess
+import time
+import webbrowser
+from urllib import request
+from urllib.request import urlopen
+import pyjokes
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import winshell
+import wolframalpha
+from ecapture import ecapture as ec
+from twilio.rest import Client
