8d49009f88f6c7f0b3e553fa7cdf0ade0083d055
Lavanya Prasanth <lavanya.prasanth103@gmail.com>
2023-01-05 09:04:17 +0530

    Code Smell fix (#15)

--- tweety_functions.py
+++ tweety_functions.py
@@ -1,39 +1,43 @@
-#import libraries required from tweety_lib_imports.py file
+# import libraries required from tweety_lib_imports.py file
 from tweety_lib_imports import *
 
-#Initialize python text to speech engine with a voice
+# Initialize python text to speech engine with a voice
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
-#Set voice ID to '0' for Male voice, '1' for Female Voice
+# Set voice ID to '0' for Male voice, '1' for Female Voice
 engine.setProperty('voice', voices[1].id)
 
-#utility functions
-#This function helps Tweety to speak
+
+# utility functions
+# This function helps Tweety to speak
 def tweety_speaks(command):
     engine.say(command)
     engine.runAndWait()
 
-#Loading the virtual assistant
+
+# Loading the virtual assistant
 def load_tweety():
     response_load = "Loading your Virtual Voice Assistant..."
     print(response_load)
     tweety_speaks(response_load)
 
-#This function helps Tweety to wish the user whenever the session starts.
+
+# This function helps Tweety to wish the user whenever the session starts.
 def wish_me():
     hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour < 12:
+    if 0 <= hour < 12:
         tweety_speaks("Hello, Good Morning, I am your virtual voice assistant tweety")
         print("Hello,Good Morning, I am your virtual voice assistant tweety")
-    elif hour >= 12 and hour < 18:
+    elif 12 <= hour < 18:
         tweety_speaks("Hello,Good Afternoon, I am your virtual voice assistant tweety")
         print("Hello,Good Afternoon, I am your virtual voice assistant tweety")
     else:
         tweety_speaks("Hello,Good Evening, I am your virtual voice assistant tweety")
         print("Hello,Good Evening, I am your virtual voice assistant tweety")
 
-#This function helps Tweety to take custom commands from user
+
+# This function helps Tweety to take custom commands from user
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -43,7 +47,7 @@ def takeCommand():
 
         try:
             print("Recognizing...")
-            user_response = r.recognize_google(audio,language='en-us')
+            user_response = r.recognize_google(audio, language='en-us')
             print(f'user command:{user_response}\n')
         except Exception as e:
             tweety_speaks("Pardon me, can you please say that again")
@@ -51,13 +55,14 @@ def takeCommand():
             takeCommand()
     return user_response
 
-#This function helps Tweety to get the user information
+
+# This function helps Tweety to get the user information
 def user_info():
     tweety_speaks("May i know your name?")
     print("May i know your name?")
     try:
         user_name = str(takeCommand())
-        if(user_name):
+        if user_name:
             tweety_speaks("Hello " + user_name)
             print("Hello " + user_name)
     except Exception as e:
@@ -66,7 +71,8 @@ def user_info():
         user_info()
     return user_name
 
-#This function helps Tweety to send an email
+
+# This function helps Tweety to send an email
 def send_email(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
@@ -77,14 +83,15 @@ def send_email(to, content):
     server.close()
     return True
 
-#This function helps to execute the user commands using webbrowser instance.
+
+# This function helps to execute the user commands using webbrowser instance.
 def execute_webbrowser(user_search, type):
     print("your search: " + user_search)
     if type == 'website':
         webbrowser.open(user_search)
         first_dot_idx = user_search.index('.')
         second_dot_idx = user_search.rindex('.')
-        website_name = user_search[first_dot_idx+1:second_dot_idx]
+        website_name = user_search[first_dot_idx + 1:second_dot_idx]
         tweety_speaks(website_name + " is open now")
         print(website_name + " is open now")
         time.sleep(5)
@@ -92,10 +99,11 @@ def execute_webbrowser(user_search, type):
         webbrowser.open(user_search)
         time.sleep(5)
 
-#This function helps to execute the user commands using webbrowser instance.
+
+# This function helps to execute the user commands using webbrowser instance.
 def execute_wolframalpha():
     query = takeCommand()
-    app_id ="your_id"
+    app_id = "your_id"
     client = wolframalpha.Client(app_id)
     res = client.query(query)
     try:
@@ -106,6 +114,7 @@ def execute_wolframalpha():
         tweety_speaks("Sorry no results found")
         print("Sorry no results found")
 
-#This function helps to execute the user commands using subprocess instance
+
+# This function helps to execute the user commands using subprocess instance
 def execute_subprocess(type):
-    subprocess.call("shutdown / h") if type == 'h' else subprocess.call(["shutdown", type]) #if else one liner
+    subprocess.call("shutdown / h") if type == 'h' else subprocess.call(["shutdown", type])  # if else one-liner
